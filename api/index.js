83483e74656cd9d1a75cf2a8a39ebd2e8d90a532
uzair171 <jabransaleem171@gmail.com>
2025-06-19 18:56:07 +0500

    connecting the database MongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express"
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(()=>{
+        console.log("Database is Connected")
+    })
+    .catch((err)=>{
+        console.log(err)
+    });
 
 const app = express();
 app.listen(3000,()=>{
-    console.log("server is online 300")
+    console.log("server is online 3000")
 })
